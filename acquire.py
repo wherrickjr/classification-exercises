b356a8fbae5519ad5b4a2eff0acdbc69af7b7c7e
Bill Herrick <wherrickjr@gmail.com>
2022-12-07 11:45:41 -0600

    none

---
+++ acquire.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import env
+
+def get_connection(db, user=env.username, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_titanic_data():
+    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
