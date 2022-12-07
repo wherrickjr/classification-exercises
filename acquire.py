873d8151dc1c147e52ff4fa8ad1618ce48100407
Bill Herrick <wherrickjr@gmail.com>
2022-12-07 16:55:42 -0600

    new functions and data prep

--- acquire.py
+++ acquire.py
@@ -1,14 +1,51 @@
 import pandas as pd
-
+import os
 import env
 
 def get_connection(db, user=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def get_titanic_data():
-    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+    filename = 'titanic.csv'
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+        df.to_csv(filename)
+        return df
 
 def get_iris_data():
-    return pd.read_sql('select measurements.*, species.species_id, \
+    filename = 'iris.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('select measurements.*, species.species_id, \
         species.species_name from species join \
             measurements using(species_id);', get_connection('iris_db'))
+        df.to_csv(filename)
+        return df
+
+def get_telco_data():
+    filename = 'telco.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+    else:
+        df = pd.read_sql('select customers.*, i.internet_service_type, \
+        c.contract_type, p.payment_type from customers\
+            join internet_service_types as i using(internet_service_type_id) \
+                join contract_types as c using(contract_type_id)\
+                     join payment_types as p using(payment_type_id);', \
+                        get_connection('telco_churn'))
+        df.to_csv(filename)
+        return df
+
+
+def prep_iris(df):
+    df = df.drop_duplicates()
+    df.drop(columns = ['species_id', 'measurement_id'], inplace = True)
+    df.rename(columns = {'species_name':'species'}, inplace = True)
+    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
