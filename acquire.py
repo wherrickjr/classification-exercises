55362ecb1a47067ed9877d51978dc2be3cb8fd91
Bill Herrick <wherrickjr@gmail.com>
2022-12-07 13:44:35 -0600

    added functions

--- acquire.py
+++ acquire.py
@@ -7,3 +7,8 @@ def get_connection(db, user=env.username, host=env.host, password=env.password):
 
 def get_titanic_data():
     return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+def get_iris_data():
+    return pd.read_sql('select measurements.*, species.species_id, \
+        species.species_name from species join \
+            measurements using(species_id);', get_connection('iris_db'))
