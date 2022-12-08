dec216b470bdf864dad662a82d2e7c8dd2014375
Bill Herrick <wherrickjr@gmail.com>
2022-12-08 10:41:30 -0600

    commit data prep exercies

--- acquire.py
+++ acquire.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import env
+from sklearn.model_selection import train_test_split
 
 def get_connection(db, user=env.username, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
@@ -49,3 +50,49 @@ def prep_iris(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
+def prep_titanic(df):
+    df = df.drop_duplicates()
+    df = df.dropna()
+    df.drop(columns = ['Unnamed: 0', 'passenger_id'\
+        , 'survived', 'pclass', 'sibsp', 'parch', 'alone', 'embarked'],\
+             inplace = True)
+    dummy_df = pd.get_dummies(df[['sex']], dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    dummy_df = pd.get_dummies(df[['class']], dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    dummy_df = pd.get_dummies(df[['deck']], dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    dummy_df = pd.get_dummies(df[['embark_town']], dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+def prep_telco(df):
+    df = df.drop_duplicates()
+    df.drop(columns = ['Unnamed: 0', 'internet_service_type_id',\
+                          'contract_type_id',\
+                          'payment_type_id',\
+                          ], inplace = True)
+    list_of_columns = ['gender', 'partner',\
+         'dependents', 'phone_service', 'multiple_lines','online_security',\
+             'online_backup', 'device_protection', 'tech_support',\
+                'streaming_movies', 'paperless_billing', 'churn', 'internet_service_type',\
+                    'contract_type', 'payment_type', 'streaming_tv']
+    dummy_df = pd.get_dummies(list_of_columns, dummy_na=False, drop_first=[True])
+    df = pd.concat([df, dummy_df], axis=1)
+    return df
+
+
+def iris_split(df):
+    train, test = train_test_split(df,test_size=.1,random_state=123, stratify=df.species)
+    train, validate = train_test_split(train,test_size=.3,random_state=123, \
+        stratify=train.species)
+
+def titanic_split(df):
+    train, test = train_test_split(df,test_size=.1,random_state=123, stratify=df.embark_town)
+    train, validate = train_test_split(train,test_size=.3,random_state=123, \
+        stratify=train.embark_town)
+
+def telco_split(df):
+    train, test = train_test_split(df,test_size=.1,random_state=123, stratify=df.churn)
+    train, validate = train_test_split(train,test_size=.3,random_state=123, \
+        stratify=train.churn)    
